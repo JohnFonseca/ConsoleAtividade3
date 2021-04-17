62d800cee8371884a3543a944973e3682dfba850
JohnFonseca <81266870+JohnFonseca@users.noreply.github.com>
2021-04-17 19:04:51 -0300

    Criação de Threads de Times
    Criação de Threads para times

--- src/main/Main.java
+++ src/main/Main.java
@@ -24,19 +24,24 @@ public class Main {
 		int elemThread = grilosNaPartida / nThreads;
 		
 		//System.out.println(grilosNaPartida);
-		ThreadProcessor threads[] = new ThreadProcessor[grilosNaPartida];
-
-		for(int i=0; i<grilosNaPartida;i++)
+		ThreadProcessor threads[];// = new ThreadProcessor[grilosNaPartida];
+		
+		int times;
+		times = grilosNaPartida/3;
+		if (grilosNaPartida%3 > 0 || times == 0)
 		{
+			times++; 
+		}
+		
+		threads= new ThreadTimes[times];
+		
+		for (int i = 0; i < nThreads; i++) {
 			int start = i * elemThread;
 			int end = start + elemThread;
-			threads[i] = new ThreadProcessor("O Grilo_"+(i+1),0,distancia,tamanhoMaximoPulo,start,end);
+			threads[i] = new ThreadTimes(i, 3, distancia, tamanhoMaximoPulo, elemThread, times);
 			threads[i].start();
 		}
 		
-		{
-			
-			
 			for (int i = 0; i < nThreads; i++) {
 				try {
 					threads[i].join();
@@ -47,8 +52,12 @@ public class Main {
 				}
 				
 			}
-		//	z++;
-		}
+			
+			System.out.println("----------");
+			
+			for (int i = 0; i < nThreads; i++) {
+				//System.out.println("O time " + i + " Total de pulos: " + threads[i].RetornaPulosTotais);
+			}
 	}
 	
 }
